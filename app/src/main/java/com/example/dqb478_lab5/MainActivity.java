45115d591b953e4a5e9093454f3f8559cf771eee
Alfonso <hipstercatz@gmail.com>
2023-11-21 19:55:15 -0600

    trying to push everything to github

--- app/src/main/java/com/example/dqb478_lab5/MainActivity.java
+++ app/src/main/java/com/example/dqb478_lab5/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button logButton = findViewById(R.id.log_button);
+    EditText usertext = findViewById(R.id.user_edit);
+    EditText passtext = findViewById(R.id.pass_edit);
+
 
 
     @Override
